bae88d019874de9a5b161b3ec4e4507abf898e1e
Athanasios Chasapis <hasapisthanasis@gmail.com>
2015-10-16 20:46:54 +0300

    First Commit with Hello World

---
+++ src/ui/Application.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ui;
+
+/**
+ * @author akanthos
+ *
+ */
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}

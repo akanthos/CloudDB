356fab6c4e98c89eb2aebcb4e55b68b75e9fe499
Sreenath Prem <srinat999@gmail.com>
2015-10-20 16:10:11 +0200

    Added logging

--- src/helpers/CannotConnectException.java
+++ src/helpers/CannotConnectException.java
@@ -5,15 +5,10 @@ public class CannotConnectException extends Exception {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-//	private String errorCode;
 	private String errorMessage; 
 	public CannotConnectException(String errorMessage) {
-//		this.errorCode = errorCode;
 		this.errorMessage = errorMessage;
 	}
-//	public String getErrorCode() {
-//		return this.errorCode;
-//	}
 	public String getErrorMessage() {
 		return this.errorMessage;
 	}

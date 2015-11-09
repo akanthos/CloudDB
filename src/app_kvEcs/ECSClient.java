57ae6e7f734bc3ee9bafc785de19484ef4b197cf
Athanasios Chasapis <hasapisthanasis@gmail.com>
2015-11-09 15:12:11 +0100

    Initial structure for Milestone 3

---
+++ src/app_kvEcs/ECSClient.java
@@ -0,0 +1,7 @@
+package app_kvEcs;
+
+/**
+ * Created by aacha on 11/9/2015.
+ */
+public class ECSClient {
+}
--- src/common/messages/KVMessage.java
+++ src/common/messages/KVMessage.java
@@ -12,7 +12,10 @@ public interface KVMessage {
 		PUT_ERROR, 		/* Put - request not successful */
 		DELETE_SUCCESS, /* Delete - request successful */
 		DELETE_ERROR, 	/* Delete - request successful */
-		GENERAL_ERROR 	/* If an unexpected situation is encountered */
+		GENERAL_ERROR, 	/* If an unexpected situation is encountered */
+		SERVER_STOPPED,         /* Server is stopped, no requests are processed */
+		SERVER_WRITE_LOCK,      /* Server locked for out, only get possible */
+		SERVER_NOT_RESPONSIBLE  /* Request not successful, server not responsible for key */
 	}
 
 	/**
